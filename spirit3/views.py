e08c6f2375371beb00f91c1fc10151ee4963494b
Kirsty <kirsty_russell@hotmail.com>
2016-06-02 16:29:23 +0100

    Set up patient list page, no patients added

--- spirit3/views.py
+++ spirit3/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Patientinfo
 
 def patient_list(request):
-	return render(request, 'spirit3/patient_list.html', {})
+	patients = Patientinfo.objects.all()
+	return render(request, 'spirit3/patient_list.html', {}) 
 
 # Create your views here.
