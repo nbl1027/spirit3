81e5721cc3390b4e8a564f6da76e5f472e48a831
Kirsty <kirsty_russell@hotmail.com>
2016-07-27 16:43:21 +0100

    Added links began working on forms

--- spirit3/urls.py
+++ spirit3/urls.py
@@ -2,8 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.patient_list, name='patient_list'), 
-	url(r'^patient/(?P<pk>\d+)/$', views.patient_detail, name='patient_detail'), 
+	url(r'^$', views.home, name='patient_list'), 
+	url(r'^patientlist', views.patient_list, name='patient_list'), 
+	url(r'^patientdetail/(?P<pk>\d+)/$', views.patient_detail, name='patient_detail'), 
+	url(r'^add_patient/$', views.add_patient, name='add_patient'),
 ]
 
 
