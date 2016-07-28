467411ae0bda67b434a5a31ddc6556fcf88272b0
Kirsty <kirsty_russell@hotmail.com>
2016-07-28 14:43:51 +0100

    Added login page and started working on restricted access

--- spirit3/urls.py
+++ spirit3/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
 	url(r'^patientlist', views.patient_list, name='patient_list'), 
 	url(r'^patientdetail/(?P<pk>\d+)/$', views.patient_detail, name='patient_detail'), 
 	url(r'^add_patient/$', views.add_patient, name='add_patient'),
+	url(r'^login/$', views.user_login, name='login'), 
+	url(r'^restricted/', views.restricted, name='restricted'),
 ]
 
 
--- spirit3/views.py
+++ spirit3/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Patientinfo
 from spirit3.forms import PatientForm
+from django.contrib.auth import authenticate, login 
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def patient_list(request):
@@ -25,3 +28,27 @@ def add_patient(request):
 	else:
 		form = PatientForm()
 	return render(request, 'spirit3/add_patient.html', {'form':form})
+
+def user_login(request):
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+		user = authenticate(username=username, password=password)
+		if user:
+			if user.is_active:
+				login(request, user)
+				return redirect('home')
+			else:
+				return HttpResponse("Your SPIRIT 3 account is disabled")
+
+		else:
+			print "Invalid login details: {0}, {1}".format(username, password)
+			return HttpResponse("Invalid login details supplied.")
+	else:
+		return render(request, 'spirit3/login.html',{})
+
+
+@login_required
+def restricted(request):	
+	return HttpResponse("Since you're logged in you can see this text")
+
